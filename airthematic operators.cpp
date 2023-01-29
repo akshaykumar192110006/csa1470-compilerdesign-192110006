39049ff315c248223d3650794473b0f5adc1dc2c
akshaykumar192110006 <112461426+akshaykumar192110006@users.noreply.github.com>
2023-01-29 12:03:11 +0530

    Add files via upload

---
+++ airthematic operators.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	 char ch;
+	 printf("enter the character:");
+	 scanf("%d",&ch);
+	 
+	if((ch= '+'' && '-' && '*' && '/'))
+	printf("arithematic operator verified",ch);
+	else
+		printf("arithematic operator not verified",ch);
+		
+		return 0;
+	 
+	 
+}
